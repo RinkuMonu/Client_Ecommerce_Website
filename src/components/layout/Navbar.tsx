d34edc1cc604eb41a0e10c42f4c077f65fb666a8
vijaysingh7878 <vijaysinghshekhawat9928@gmail.com>
2025-08-26 15:50:48 +0530

    edit

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -467,7 +467,7 @@ const Navbar: React.FC<NavbarProps> = ({ onCartClick, cartItemCount }) => {
                       </div>
 
                       {/* Hover Tooltip */}
-                      <div className="absolute top-full mt-1 left-0 w-50 bg-[#FFF3FD] border border-gray-100 text-gray-800 rounded-lg shadow-xl opacity-0 scale-95 group-hover:opacity-100 group-hover:scale-100 transform origin-top transition-all duration-200 ease-out z-50">
+                      <div className="absolute top-full mt-1 left-0 w-50 bg-[#FFF3FD] border border-gray-100 text-gray-800 rounded-lg shadow-xl hidden scale-95 group-hover:flex group-hover:scale-100 transform origin-top transition-all duration-200 ease-out z-50">
                         <ul className="py-1">
                           {items.map((item) => (
                             <li key={item._id}>
